5ac858aa483207b4fcf8be0aeb47df46e335d5e4
sh3ngsh3ng <yshenglim13@gmail.com>
2022-03-11 08:12:07 +0000

    s

--- my-app/src/app/components/button/button.component.ts
+++ my-app/src/app/components/button/button.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   @Input() item = ""
-
+  @Input() target = ""
   constructor() { }
 
   ngOnInit(): void {
