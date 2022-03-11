3cb13a421972d552faf59b1f70a6baa06dc50581
sh3ngsh3ng <yshenglim13@gmail.com>
2022-03-11 06:55:20 +0000

    added button component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
